e39316073ea423f6ab1a62cc286fc30a2a64b897
Maycon Felipe <mayconfelipe164@gmail.com>
2021-03-19 13:18:47 -0300

    metodo realiza operaçoes

--- ExercicioThreadSemaforo1/src/controller/ThreadProcessamento.java
+++ ExercicioThreadSemaforo1/src/controller/ThreadProcessamento.java
@@ -19,34 +19,34 @@ public class ThreadProcessamento extends Thread {
 		int resto = id % 3;
 
 		if (resto == 1) {
-			for (int vezes = 0; vezes < 2; vezes++) {
-				int tempo = (int) (Math.random() * 801) + 200;
-				calculos(tempo);
-				transacao(1000);
-			}
+			int tempo = (int) (Math.random() * 801) + 200;
+			realizarOperacoes(2, tempo, 1000);
+		
 		} else if (resto == 2) {
-
-			for (int vezes = 0; vezes < 3; vezes++) {
-				int tempo = (int) (Math.random() * 1001) + 500;
-				calculos(tempo);
-				transacao(1500);
-			}
-
+			
+			int tempo = (int) (Math.random() * 1001) + 500;
+			realizarOperacoes(3, tempo, 1500);
+		
 		} else {
-			for (int vezes = 0; vezes < 3; vezes++) {
-				int tempo = (int) (Math.random() * 1001) + 1000;
-				calculos(tempo);
-				transacao(1500);
-			}
+			
+			int tempo = (int) (Math.random() * 1001) + 1000;
+			realizarOperacoes(3, tempo, 1500);
+			
 		}
 	}
 
+	private void realizarOperacoes(int n, int tempo, int tempoTransacao) {
+		for (int vezes = 0; vezes < n; vezes++) {	
+			calculos(tempo);
+			transacao(tempoTransacao);
+		}
+	}
+	
 	private void calculos(int tempo) {
 		try {
 			System.out.println("Thread #" + id + " processando....");
 			sleep(tempo);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -57,7 +57,6 @@ public class ThreadProcessamento extends Thread {
 			System.out.println("Thread #" + id + " transação de BD...");
 			sleep(tempo);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			sema.release();
